a67258c59ffe5dad5b496ac54073260d3a903de2
dongha <ssyy3034@naver.com>
2025-05-26 13:18:23 +0900

    workflow test commit

--- src/main/java/ding/co/backendportfolio/BackendportfolioApplication.java
+++ src/main/java/ding/co/backendportfolio/BackendportfolioApplication.java
@@ -9,7 +9,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class BackendportfolioApplication {
+
     public static void main(String[] args) {
+        System.out.println("화이팅!");
         SpringApplication.run(BackendportfolioApplication.class, args);
     }
 
